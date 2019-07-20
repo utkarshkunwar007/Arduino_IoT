0f19c7328c73fbab052300ff87bfc14876c8a3f0
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-20 12:58:04 +0530

    Add files via upload

---
+++ addition.ino
@@ -0,0 +1,34 @@
+int a;
+int b;
+int c;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+a=10;
+b=20;
+c=a+b;
+Serial.print("Input A= ");
+Serial.println(a);
+Serial.print("Input B= ");
+Serial.println(b);
+Serial.print("\nAddition: ");
+Serial.println(c);
+delay(5000);
+c=b-a;
+Serial.print("Substraction: ");
+Serial.println(c);
+delay(5000);
+c=b*a;
+Serial.print("Multiplication: ");
+Serial.println(c);
+delay(5000);
+c=b/a;
+Serial.print("Division: ");
+Serial.println(c);
+delay(5000);
+}
