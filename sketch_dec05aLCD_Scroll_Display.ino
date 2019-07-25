4d970305c28fb44c3c14af6a74bd2e70176b7272
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:07:50 +0530

    Add files via upload

---
+++ sketch_dec05aLCD_Scroll_Display.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+//String data;
+//int len;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Utkarsh Kunwar Singh is the freelance IoT Trainer.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i <= 16; i++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(1000);
+  }
+}
