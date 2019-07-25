baf7d034b2744ab6e0c12af298c182d135ab3aa8
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:06:04 +0530

    Add files via upload

---
+++ serialport_string_read.ino
@@ -0,0 +1,15 @@
+String value;
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);    // set the baud rate to 9600, same should be of your Serial Monitor
+}
+
+void loop()
+{
+  if (Serial.available()) {
+    value = Serial.readString(); //read the input
+    Serial.println(value);
+    //Serial.print("\n");
+  }
+}
+
