f89a3b65faec53278000889c5b1ebd69bca685a7
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:11:51 +0530

    Add files via upload

---
+++ traffic_light_.ino
@@ -0,0 +1,18 @@
+int red = 2;
+int yellow = 3;
+int green = 4;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
+  delay(2000);
+
+}
