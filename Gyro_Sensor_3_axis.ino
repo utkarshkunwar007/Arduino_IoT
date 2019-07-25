be0b5862eb9ae9aeb8e0e978c7aa65e7110cad06
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:02:16 +0530

    Add files via upload

---
+++ Gyro_Sensor_3_axis.ino
@@ -0,0 +1,26 @@
+
+int x = 0;
+int y = 0;
+int z = 0;
+
+void setup() {
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  x = analogRead(A0);
+  y = analogRead(A1);
+  z = analogRead(A2);
+  Serial.print("x=");
+  Serial.print(x);
+  Serial.print(", y=");
+  Serial.print(y);
+  Serial.print(", z=");
+  Serial.print(z);
+  Serial.print("\n\n");
+  delay(2000);
+}
