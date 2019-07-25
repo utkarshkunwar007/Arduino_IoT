09c9ede745b716f38d68fdfb42cfafec15ad80c8
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:11:12 +0530

    Add files via upload

---
+++ sketch_jan05b_SOIL_Moisture_Sensor_Digital.ino
@@ -0,0 +1,20 @@
+int led_pin = 13;
+int sensor_pin = 7;
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+  pinMode(sensor_pin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (digitalRead(sensor_pin) == HIGH) {
+    digitalWrite(led_pin, HIGH);
+    Serial.print(sensor_pin);
+  } else {
+    digitalWrite(led_pin, LOW);
+    Serial.print(sensor_pin);
+    delay(1000);
+  }
+}
+
