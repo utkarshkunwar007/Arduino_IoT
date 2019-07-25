33bc1aa54a536833c70b6a17150d74068c8469ad
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:05:41 +0530

    Add files via upload

---
+++ serial_port_com.ino
@@ -0,0 +1,32 @@
+String data;
+int pin = 12;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available())
+  {
+    data = Serial.readString();
+    data.trim();
+    Serial.println("Data is : " + data);
+    if (data == "on")
+    {
+      digitalWrite(pin, HIGH);
+      Serial.println("LED ON");
+    }
+    if (data == "off")
+    {
+      digitalWrite(pin, LOW);
+      Serial.println("LED OFF");
+    }
+    if ((data != "on") && (data != "off"))
+    {
+      digitalWrite(pin, LOW);
+      Serial.println("UnKnown Command");
+    }
+  }
+}
