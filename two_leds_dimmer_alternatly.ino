3effde2f9a29bc6139dd4a4166f906ab48cd284c
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:12:47 +0530

    Add files via upload

---
+++ two_leds_dimmer_alternatly.ino
@@ -0,0 +1,30 @@
+void setup() {
+  pinMode(3, OUTPUT);
+pinMode(5, OUTPUT);
+}
+
+void loop() {
+
+  for (int i = 0; i <= 255; i++)
+  {
+    analogWrite(3, i);
+    delay(10);
+  }
+  for (int i = 255; i >= 0; i--)
+  {
+    analogWrite(5, i);
+    delay(10);
+  }
+  delay(1000);
+ for (int i = 0; i <= 255; i++)
+  {
+    analogWrite(5, i);
+    delay(10);
+  }
+  for (int i = 255; i >= 0; i--)
+  {
+    analogWrite(3, i);
+    delay(10);
+  }
+   delay(1000);
+}
