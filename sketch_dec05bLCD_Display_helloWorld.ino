9a9dc01af568d3ce1d9f563635fd24f7f89c4010
Utkarsh Kunwar <43289696+utkarshkunwar007@users.noreply.github.com>
2019-07-25 11:08:21 +0530

    Add files via upload

---
+++ sketch_dec05bLCD_Display_helloWorld.ino
@@ -0,0 +1,15 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2); // RS,E,D4,D5,D6,D7
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin(16,2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+lcd.setCursor(0,0);
+lcd.print("Hello, World! ");
+lcd.setCursor(0,1);
+lcd.print("Utkarsh Kunwar ");
+}
